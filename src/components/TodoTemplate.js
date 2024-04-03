d2a0021890902b5a6437825438d22c2b20441a8c
rnjs9861 <rkwkvlrmakfh@gmail.com>
2024-04-03 13:03:06 +0900

    여러개 바꿈

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "../styles/TodoTemplate.scss"
+import "../styles/TodoTemplate.scss";
 
-const TodoTemplate = ({children}) => {
-    return (
-        <div className="TodoTemplate">
-            <div className="app-title">일정관리</div>
-            <div className="content">{children}</div>
-        </div>
-    );
+const TodoTemplate = ({ children }) => {
+  return (
+    <div className="TodoTemplate">
+      <div className="app-title">일정관리</div>
+      <div className="content">{children}</div>
+    </div>
+  );
 };
-//한줄이면 return <> 하면 되는데 두 줄 이상일시 ()필수
+
 export default TodoTemplate;
